a1180e01c7321dfe0ba7a7fc829b2d9255599ea4
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-08-14 01:22:54 +0800

    Using hash params

--- tfaim_web.rb
+++ tfaim_web.rb
@@ -52,118 +52,49 @@ end
 
 get '/food_concentration/result/:type' do
   @full_food = full_food
-  type = params['type']
-  case type
+  params_baby     = calc_baby(t)
+  params_kid      = calc_kid(t)
+  params_child    = calc_child(t)
+  params_teenager = calc_teenager(t)
+  params_adult    = calc_adult(t)
+  params_older    = calc_older(t)
+  params_elder    = calc_elder(t)
+
+  case params['type']
   when 'baby'
-    @title        = '嬰兒攝食量評估表'
-    @use_avg      = t.baby_use_avg
-    @use_avg_sum  = sum(@use_avg.flatten)
-    @use_high     = t.baby_use_high
-    @use_max    = @use_high.flatten.max
-    @cos_intake = find_cos(@use_avg, @use_max, @use_high)
-    @result     = @use_avg_sum + @use_max - @cos_intake
+    @title = '嬰兒攝食量評估表'
+
+    @params = params_baby
     erb :under, layout: :result_layout
   when 'kid'
-    @title       = '幼兒攝食量評估表'
-    @use_avg     = t.kid_use_avg
-    @use_avg_sum = sum(@use_avg.flatten)
-    @use_high    = t.kid_use_high
-    @use_max    = @use_high.flatten.max
-    @cos_intake = find_cos(@use_avg, @use_max, @use_high)
-    @result     = @use_avg_sum + @use_max - @cos_intake
+    @title = '幼兒攝食量評估表'
+
+    @params = params_kid
     erb :under, layout: :result_layout
   when 'child'
-    @title       = '孩童攝食量評估表'
-    @use_avg     = t.child_use_avg
-    @use_avg_sum = sum(@use_avg.flatten)
-    @use_high    = t.child_use_high
-    @use_max    = @use_high.flatten.max
-    @cos_intake = find_cos(@use_avg, @use_max, @use_high)
-    @result     = @use_avg_sum + @use_max - @cos_intake
+    @title = '孩童攝食量評估表'
+
+    @params = params_child
     erb :under, layout: :result_layout
   when 'teenager'
-    @title              = '青少年攝食量評估表'
-    @male_use_avg       = t.teenager_male_use_avg
-    @male_use_avg_sum   = sum(@male_use_avg.flatten)
-    @male_use_high      = t.teenager_male_use_high
-    @female_use_avg     = t.teenager_female_use_avg
-    @female_use_avg_sum = sum(@female_use_avg.flatten)
-    @female_use_high    = t.teenager_female_use_high
-    @use_avg            = t.teenager_use_avg
-    @use_avg_sum        = sum(@use_avg.flatten)
-    @use_high           = t.teenager_use_high
-    @male_use_max      = @male_use_high.flatten.max
-    @female_use_max    = @female_use_high.flatten.max
-    @male_cos_intake   = find_cos(@male_use_avg, @male_use_max, @male_use_high)
-    @female_cos_intake = find_cos(@female_use_avg, @female_use_max, @female_use_high)
-    @male_result       = @male_use_avg_sum + @male_use_max - @male_cos_intake
-    @female_result     = @female_use_avg_sum + @female_use_max - @female_cos_intake
-    @use_max    = @use_high.flatten.max
-    @cos_intake = find_cos(@use_avg, @use_max, @use_high)
-    @result     = @use_avg_sum + @use_max - @cos_intake
+    @title = '青少年攝食量評估表'
+
+    @params = params_teenager
     erb :normal, layout: :result_layout
   when 'adult'
-    @title              = '成年人攝食量評估表'
-    @male_use_avg       = t.adult_male_use_avg
-    @male_use_avg_sum   = sum(@male_use_avg.flatten)
-    @male_use_high      = t.adult_male_use_high
-    @female_use_avg     = t.adult_female_use_avg
-    @female_use_avg_sum = sum(@female_use_avg.flatten)
-    @female_use_high    = t.adult_female_use_high
-    @use_avg            = t.adult_use_avg
-    @use_avg_sum        = sum(@use_avg.flatten)
-    @use_high           = t.adult_use_high
-    @male_use_max      = @male_use_high.flatten.max
-    @female_use_max    = @female_use_high.flatten.max
-    @male_cos_intake   = find_cos(@male_use_avg, @male_use_max, @male_use_high)
-    @female_cos_intake = find_cos(@female_use_avg, @female_use_max, @female_use_high)
-    @male_result       = @male_use_avg_sum + @male_use_max - @male_cos_intake
-    @female_result     = @female_use_avg_sum + @female_use_max - @female_cos_intake
-    @use_max    = @use_high.flatten.max
-    @cos_intake = find_cos(@use_avg, @use_max, @use_high)
-    @result     = @use_avg_sum + @use_max - @cos_intake
+    @title = '成年人攝食量評估表'
+
+    @params = params_adult
     erb :normal, layout: :result_layout
   when 'older'
-    @title              = '老年人攝食量評估表'
-    @male_use_avg       = t.older_male_use_avg
-    @male_use_avg_sum   = sum(@male_use_avg.flatten)
-    @male_use_high      = t.older_male_use_high
-    @female_use_avg     = t.older_female_use_avg
-    @female_use_avg_sum = sum(@female_use_avg.flatten)
-    @female_use_high    = t.older_female_use_high
-    @use_avg            = t.older_use_avg
-    @use_avg_sum        = sum(@use_avg.flatten)
-    @use_high           = t.older_use_high
-    @male_use_max      = @male_use_high.flatten.max
-    @female_use_max    = @female_use_high.flatten.max
-    @male_cos_intake   = find_cos(@male_use_avg, @male_use_max, @male_use_high)
-    @female_cos_intake = find_cos(@female_use_avg, @female_use_max, @female_use_high)
-    @male_result       = @male_use_avg_sum + @male_use_max - @male_cos_intake
-    @female_result     = @female_use_avg_sum + @female_use_max - @female_cos_intake
-    @use_max    = @use_high.flatten.max
-    @cos_intake = find_cos(@use_avg, @use_max, @use_high)
-    @result     = @use_avg_sum + @use_max - @cos_intake
+    @title = '老年人攝食量評估表'
+
+    @params = params_older
     erb :normal, layout: :result_layout
   when 'elder'
-    @title              = '高齡老年人攝食量評估表'
-    @male_use_avg       = t.elder_male_use_avg
-    @male_use_avg_sum   = sum(@male_use_avg.flatten)
-    @male_use_high      = t.elder_male_use_high
-    @female_use_avg     = t.elder_female_use_avg
-    @female_use_avg_sum = sum(@female_use_avg.flatten)
-    @female_use_high    = t.elder_female_use_high
-    @use_avg            = t.elder_use_avg
-    @use_avg_sum        = sum(@use_avg.flatten)
-    @use_high           = t.elder_use_high
-    @male_use_max      = @male_use_high.flatten.max
-    @female_use_max    = @female_use_high.flatten.max
-    @male_cos_intake   = find_cos(@male_use_avg, @male_use_max, @male_use_high)
-    @female_cos_intake = find_cos(@female_use_avg, @female_use_max, @female_use_high)
-    @male_result       = @male_use_avg_sum + @male_use_max - @male_cos_intake
-    @female_result     = @female_use_avg_sum + @female_use_max - @female_cos_intake
-    @use_max    = @use_high.flatten.max
-    @cos_intake = find_cos(@use_avg, @use_max, @use_high)
-    @result     = @use_avg_sum + @use_max - @cos_intake
+    @title = '高齡老年人攝食量評估表'
+
+    @params = params_elder
     erb :normal, layout: :result_layout
   when 'EDI'
     @title = '攝食暴露量估計總表'
@@ -172,6 +103,125 @@ get '/food_concentration/result/:type' do
     @title = '暴露量估計總表'
     erb :ADI, layout: :result_layout
   end
+end
+
+def calc_baby(t)
+  params = {}
+
+  params[:use_avg]     = t.baby_use_avg
+  params[:use_avg_sum] = sum(params[:use_avg].flatten)
+  params[:use_high]    = t.baby_use_high
+  calc_under(params)
+end
+
+def calc_kid(t)
+  params = {}
+
+  params[:use_avg]     = t.kid_use_avg
+  params[:use_avg_sum] = sum(params[:use_avg].flatten)
+  params[:use_high]    = t.kid_use_high
+  calc_under(params)
+end
+
+def calc_child(t)
+  params = {}
+
+  params[:use_avg]     = t.child_use_avg
+  params[:use_avg_sum] = sum(params[:use_avg].flatten)
+  params[:use_high]    = t.child_use_high
+  calc_under(params)
+end
+
+def calc_teenager(t)
+  params = {}
+
+  params[:male_use_avg]       = t.teenager_male_use_avg
+  params[:male_use_avg_sum]   = sum(params[:male_use_avg].flatten)
+  params[:male_use_high]      = t.teenager_male_use_high
+  params[:female_use_avg]     = t.teenager_female_use_avg
+  params[:female_use_avg_sum] = sum(params[:female_use_avg].flatten)
+  params[:female_use_high]    = t.teenager_female_use_high
+  params[:use_avg]            = t.teenager_use_avg
+  params[:use_avg_sum]        = sum(params[:use_avg].flatten)
+  params[:use_high]           = t.teenager_use_high
+  params[:male_use_max]       = params[:male_use_high].flatten.max
+  params[:female_use_max]     = params[:female_use_high].flatten.max
+  params[:male_cos_intake]    = find_cos(params[:male_use_avg], params[:male_use_max], params[:male_use_high])
+  params[:female_cos_intake]  = find_cos(params[:female_use_avg], params[:female_use_max], params[:female_use_high])
+  calc_upper(params)
+end
+
+def calc_adult(t)
+  params = {}
+
+  params[:male_use_avg]       = t.adult_male_use_avg
+  params[:male_use_avg_sum]   = sum(params[:male_use_avg].flatten)
+  params[:male_use_high]      = t.adult_male_use_high
+  params[:female_use_avg]     = t.adult_female_use_avg
+  params[:female_use_avg_sum] = sum(params[:female_use_avg].flatten)
+  params[:female_use_high]    = t.adult_female_use_high
+  params[:use_avg]            = t.adult_use_avg
+  params[:use_avg_sum]        = sum(params[:use_avg].flatten)
+  params[:use_high]           = t.adult_use_high
+  params[:male_use_max]       = params[:male_use_high].flatten.max
+  params[:female_use_max]     = params[:female_use_high].flatten.max
+  params[:male_cos_intake]    = find_cos(params[:male_use_avg], params[:male_use_max], params[:male_use_high])
+  params[:female_cos_intake]  = find_cos(params[:female_use_avg], params[:female_use_max], params[:female_use_high])
+  calc_upper(params)
+end
+
+def calc_older(t)
+  params = {}
+
+  params[:male_use_avg]       = t.older_male_use_avg
+  params[:male_use_avg_sum]   = sum(params[:male_use_avg].flatten)
+  params[:male_use_high]      = t.older_male_use_high
+  params[:female_use_avg]     = t.older_female_use_avg
+  params[:female_use_avg_sum] = sum(params[:female_use_avg].flatten)
+  params[:female_use_high]    = t.older_female_use_high
+  params[:use_avg]            = t.older_use_avg
+  params[:use_avg_sum]        = sum(params[:use_avg].flatten)
+  params[:use_high]           = t.older_use_high
+  params[:male_use_max]       = params[:male_use_high].flatten.max
+  params[:female_use_max]     = params[:female_use_high].flatten.max
+  params[:male_cos_intake]    = find_cos(params[:male_use_avg], params[:male_use_max], params[:male_use_high])
+  params[:female_cos_intake]  = find_cos(params[:female_use_avg], params[:female_use_max], params[:female_use_high])
+  calc_upper(params)
+end
+
+def calc_elder(t)
+  params = {}
+
+  params[:male_use_avg]       = t.elder_male_use_avg
+  params[:male_use_avg_sum]   = sum(params[:male_use_avg].flatten)
+  params[:male_use_high]      = t.elder_male_use_high
+  params[:female_use_avg]     = t.elder_female_use_avg
+  params[:female_use_avg_sum] = sum(params[:female_use_avg].flatten)
+  params[:female_use_high]    = t.elder_female_use_high
+  params[:use_avg]            = t.elder_use_avg
+  params[:use_avg_sum]        = sum(params[:use_avg].flatten)
+  params[:use_high]           = t.elder_use_high
+  params[:male_use_max]       = params[:male_use_high].flatten.max
+  params[:female_use_max]     = params[:female_use_high].flatten.max
+  params[:male_cos_intake]    = find_cos(params[:male_use_avg], params[:male_use_max], params[:male_use_high])
+  params[:female_cos_intake]  = find_cos(params[:female_use_avg], params[:female_use_max], params[:female_use_high])
+  calc_upper(params)
+end
+
+def calc_under(params)
+  params[:use_max]    = params[:use_high].flatten.max
+  params[:cos_intake] = find_cos(params[:use_avg], params[:use_max], params[:use_high])
+  params[:result]     = params[:use_avg_sum] + params[:use_max] - params[:cos_intake]
+  params
+end
+
+def calc_upper(params)
+  params[:male_result]   = params[:male_use_avg_sum] + params[:male_use_max] - params[:male_cos_intake]
+  params[:female_result] = params[:female_use_avg_sum] + params[:female_use_max] - params[:female_cos_intake]
+  params[:use_max]       = params[:use_high].flatten.max
+  params[:cos_intake]    = find_cos(params[:use_avg], params[:use_max], params[:use_high])
+  params[:result]        = params[:use_avg_sum] + params[:use_max] - params[:cos_intake]
+  params
 end
 
 def take_input(params)
